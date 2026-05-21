78909a0c2114a944bfe9ee5de5ca0a9a52e2bb4d
SmartStore Dev <dev@smartstore.local>
2026-05-20 20:05:39 -0600

    Add populate_users command to auto-create test users

---
+++ backend/apps/users/management/commands/populate_users.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Populate default users'
+
+    def handle(self, *args, **options):
+        users_data = [
+            {'email': 'gomezmartinez052@gmail.com', 'username': 'juan_gomez', 'password': 'Test123!', 'first_name': 'Juan', 'last_name': 'Gomez'},
+            {'email': 'jgomezmartinez1997@gmail.com', 'username': 'guillermo_gomez', 'password': 'Test123!', 'first_name': 'Guillermo', 'last_name': 'Gomez'},
+            {'email': 'testcheckout@test.com', 'username': 'test_user', 'password': 'Test123!', 'first_name': 'Test', 'last_name': 'User'},
+        ]
+
+        for user_data in users_data:
+            if not User.objects.filter(email=user_data['email']).exists():
+                User.objects.create_user(**user_data)
+                self.stdout.write(self.style.SUCCESS(f"OK Usuario {user_data['email']} creado"))
+            else:
+                self.stdout.write(self.style.WARNING(f"SKIP Usuario {user_data['email']} ya existe"))
